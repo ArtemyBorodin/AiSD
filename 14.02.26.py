0e3ce3e0b696f2d9d8b148cb6e3d03780ed77f87
Artemy <art_borodin@mail.ru>
2026-02-20 16:36:01 +0300

    добавлены новые файлы

---
+++ 14.02.26.py
@@ -0,0 +1,65 @@
+#1
+# import numpy as np
+# arr1 = np.array([1,2,3,4,5,6,7,8,9,10])
+# print(arr1)
+
+#2
+# import numpy as np
+# arr1 = np.zeros(7)
+# print(arr1)
+
+#3
+# import numpy as np
+# arr1 = np.ones(5)
+# print(arr1)
+
+#4
+# import numpy as np
+# arr_range = np.arange(10,50,5)
+# print(arr_range)
+
+#5
+# import numpy as np
+# arr_range = np.arange(0,10)
+# print(arr_range[1])
+# print(arr_range[-1])
+# print(arr_range[3])
+# print(arr_range[6])
+
+#6
+# import numpy as np
+# arr1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
+# for i in arr1:
+#     print(i)
+
+#7
+# import numpy as np
+# arr1 =  np.array([1,2,3,4,5])
+# arr2 = np.array([6,7,8,9,1])
+# arr_sum = np.add(arr1,arr2)
+# print(arr_sum)
+
+#8
+# import numpy as np
+# arr1 = np.array([12,3,45,5])
+# arr_multy = np.multiply(arr1,2)
+# print(arr_multy)
+
+#9
+# import numpy as np
+# arr1 = np.array([1,2,3,4,5])
+# arr_square = arr1 ** 2
+# print(arr_square)
+
+#10 функций np.min() и np.max().
+# import numpy as np
+# arr1 = np.array([1,2,3,4,5])
+# arr_min = np.min(arr1)
+# arr_max = np.max(arr1)
+# print(arr_min)
+# print(arr_max)
+
+# import numpy as np
+# arr1 = np.array([1,2,3,4,5,6,7,8,9])
+# arr_condition = arr1 > 5 # arr_condition булевая переменная True or False
+# print(arr_condition)
